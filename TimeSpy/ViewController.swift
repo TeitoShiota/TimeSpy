cc55fb47dcb852b00abc7956982a26049d175585
Lasse Karahan Kristiansen <lasselaflair@gmail.com>
2020-12-14 16:46:17 +0100

    Draft v0.3 - Update all code.

--- TimeSpy/ViewController.swift
+++ TimeSpy/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         workHourStartStepperOutlet.stepValue = 0.25
         workHourEndStepperOutlet.stepValue = 0.25
+        workHourStartLabel.text = String(workHourStartStepperOutlet.value)
+        workHourEndLabel.text = String(workHourEndStepperOutlet.value)
     }
 
 //Global Variabals
@@ -34,14 +35,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultsLabel: UILabel!
     
 //Buttons
-    @IBAction func calculateHoursButton(_ sender: Any) {updateResultsLabel(newText: String(calculateHours()))}
+    @IBAction func calculateHoursButton(_ sender: Any) {updateResultsLabel()}
     
 //Functions
+    
     func calculateHours() -> Float{
-        return Float(workHourEndStepperOutlet.value - workHourStartStepperOutlet.value - 0.5)
+        var result:Float = Float(workHourEndStepperOutlet.value - workHourStartStepperOutlet.value)
+        if workHourEndStepperOutlet.value >= 12.5{
+            result -= 0.5
+        } else if workHourEndStepperOutlet.value >= 12.25{
+            result -= 0.25
+        }
+        return result
     }
-    func updateResultsLabel(newText:String){
-        resultsLabel.text = "You have worked for \(calculateHours()) hours"
+    
+    func updateResultsLabel(){
+        resultsLabel.text = "You have worked for \(Int(calculateHours()))\(convertDecimalHoursToString(calculateHours())) hours"
+    }
+    
+    //Takes work hours as input and returns a containing the floating point fraction of hours converted into a matching text
+    func convertDecimalHoursToString(_ var1:Float) -> String {
+        var result:String = ""
+        let hoursInFractions: Float = (var1 - floor(var1))
+        switch hoursInFractions {
+        case 0.25:
+            result = " and 15 minutes"
+        case 0.5:
+            result = " and 30 minutes"
+        case 0.75:
+            result = " and 45 minutes"
+        default:
+            result = ""
+        }
+        return result
     }
 }
 
